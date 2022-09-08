3ff4dc27765d8dd5aae27d8ccd8cad1c031b5404
Jose Guaman <jeguaman1@gmail.com>
2022-09-08 02:20:42 -0500

    Initial Structure for Technical Interview Proposal

---
+++ src/main/java/com/ec/interview/sicpa/SicpaApplication.java
@@ -0,0 +1,13 @@
+package com.ec.interview.sicpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicpaApplication.class, args);
+	}
+
+}
